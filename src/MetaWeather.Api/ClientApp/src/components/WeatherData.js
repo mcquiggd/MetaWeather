17613ba6035c6f67188803a4e623054b286ea2ea
David Paul McQuiggin <david@pro-coded.com>
2020-04-28 10:49:27 +0100

    Added Pull To Refresh

--- src/MetaWeather.Api/ClientApp/src/components/WeatherData.js
+++ src/MetaWeather.Api/ClientApp/src/components/WeatherData.js
@@ -1,20 +1,44 @@
 import React, { Component } from 'react';
+import ReactPullToRefresh from 'react-pull-to-refresh';
 import authService from './api-authorization/AuthorizeService'
+import { Log } from 'oidc-client';
 
 export class WeatherData extends Component {
   static displayName = WeatherData.name;
 
   constructor(props) {
     super(props);
-    this.state = { locations: [], loading: true };
-  }
+      this.state = { locations: [], loading: true };
+      this.handleRefresh = this.handleRefresh.bind(this);
+    }
 
   componentDidMount() {
     this.populateWeatherData();
-  }
+    }
+
+    async populateWeatherData() {
+        console.log('refreshing');
+        const token = await authService.getAccessToken();
+
+        const response = await fetch('api/weather', {
+            method: 'post',
+            body: JSON.stringify({ woeId: 44544 }),
+            headers: !token ? { 'Content-Type': 'application/json' } : { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` }
+        });
+
+        const data = await response.json();
+        this.setState({ forecasts: data.forecasts, loading: false });
+    }
+
+    handleRefresh(resolve, reject) {
+        let self = this;
+        setTimeout(function () {
+        self.populateWeatherData() ? resolve() : reject();
+        }, 500);
+    }
 
   static renderForecastsTable(forecasts) {
-    return (
+      return (          
       <table className='table table-striped' aria-labelledby="tabelLabel">
         <thead>
           <tr>
@@ -32,7 +56,7 @@ export class WeatherData extends Component {
             </tr>
           )}
         </tbody>
-      </table>
+              </table>
     );
   }
 
@@ -44,22 +68,17 @@ export class WeatherData extends Component {
     return (
       <div>
         <h1 id="tabelLabel">Weather Forecasts</h1>
-        <p>This component demonstrates fetching weather forecast data from the server.</p>
-        {contents}
+            <p>This component demonstrates fetching weather forecast data from the server.</p>
+            <ReactPullToRefresh
+                onRefresh={this.handleRefresh}
+                className="your-own-class-if-you-want"
+                style={{
+                    textAlign: 'center'
+                }}>
+                <h3>Pull down to refresh</h3>
+                {contents}
+            </ReactPullToRefresh>
       </div>
     );
-  }
-
-  async populateWeatherData() {
-    const token = await authService.getAccessToken();
-
-      const response = await fetch('api/weather', {
-              method: 'post',
-          body: JSON.stringify({ woeId: 44544}),              
-          headers: !token ? { 'Content-Type': 'application/json'} : { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` }
-      });
-
-    const data = await response.json();
-    this.setState({ forecasts: data.forecasts, loading: false });
   }
 }
